4b695a514c8be32b03e740432f37db07144e3a4c
Shruti <shrutipandeyofficial@gmail.com>
2025-08-20 15:46:42 +0530

    Add layers and create simple account creation function

---
+++ banking-app/src/main/java/com/project/banking_app/entity/Account.java
@@ -0,0 +1,31 @@
+package com.project.banking_app.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="accounts")
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="account_number")
+    private Long accountNumber;
+
+    @Column(name="account_holder_name")
+    private String accountHolderName;
+
+    private double balance;
+}
